2159d5246ab3da989a6cff98afe116b56bf6a783
Sabari <sabarikrishnavk@gmail.com>
2025-04-12 05:26:25 +1000

    user service

---
+++ user-service/src/main/java/com/sellaway/userservice/event/UserEvent.java
@@ -0,0 +1,13 @@
+package com.sellaway.userservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEvent {
+    private String eventType;
+    private Long userId; 
+}
